b1a77a0eef03e0a4ab05489950dc10521f58659e
Кавпуш Максим Владимирович <mvkavpush@mts.ru>
2022-09-21 16:02:32 +0300

    rewrite clean

--- gclean.cmd
+++ gclean.cmd
@@ -1,1 +1,1 @@
-@git branch | findstr /v "* ma" | xargs -r git branch -D
+@(git checkout main||git cheeckout master) && git fetch --all -pPf && git branch -av | awk  '/\[gone\]/{print $1}'|xargs -r git branch -D
