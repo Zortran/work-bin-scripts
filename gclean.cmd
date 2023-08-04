5d08b80f91832cb9018549761376cb73ab308831
Кавпуш Максим Владимирович <MVKavpush@MTS.ru>
2023-08-04 10:52:36 +0300

    gclean tune

--- gclean.cmd
+++ gclean.cmd
@@ -1,1 +1,2 @@
-@git branch | findstr /v "* master" | xargs -r git branch -D
+(git branch -v | sls \[gone\]) | foreach {git branch -D ($_ -split '\s+')[1]}
+::@git branch | findstr /v "* master" | xargs -r git branch -D
