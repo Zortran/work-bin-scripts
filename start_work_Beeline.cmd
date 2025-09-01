2ac2ffa278581a823f2523954ea2572b819a16b5
Maksim Kavpush <mkavpush@beeline.ru>
2025-09-01 15:49:50 +0300

    sw off BlueMail

--- start_work_Beeline.cmd
+++ start_work_Beeline.cmd
@@ -13,4 +13,4 @@ taskkill /f /t /im pageant.exe
 del %USERPROFILE%\.ssh\ssh-sock.sock
 start /b "pageant.exe" "C:\Program Files\PuTTY\pageant.exe" --unix %USERPROFILE%\.ssh\ssh-sock.sock --openssh-config %USERPROFILE%\.ssh\pageant.conf > nul
 
-explorer.exe shell:AppsFolder\BlueMail.BlueMailEmail_t08282y3j4hc4!BlueMail.BlueMailEmail
+:: explorer.exe shell:AppsFolder\BlueMail.BlueMailEmail_t08282y3j4hc4!BlueMail.BlueMailEmail
