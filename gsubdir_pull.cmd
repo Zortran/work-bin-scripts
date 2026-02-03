b883b9bef4534a430467db0ddf5a48b9eec1bc30
Maksim Kavpush <mkavpush@beeline.ru>
2026-02-03 11:51:38 +0300

    tune gsubdir_pull.cmd

--- gsubdir_pull.cmd
+++ gsubdir_pull.cmd
@@ -1,1 +1,1 @@
-@pwsh -command "Get-ChildItem -Directory -Force -Recurse *.git | ForEach-Object -Parallel { cd $($_.Parent); $LOG=&{ echo ""$($_.Parent)""; git pull }; write $LOG }"
+@pwsh -command "Get-ChildItem -Directory -Force -Recurse *.git | ForEach-Object -Parallel { cd $($_.Parent); $LOG=&{ echo ""$($_.Parent)""; git pull --rebase --all --prune --tags }; write $LOG }"
