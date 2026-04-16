03f1d659d09d0c33740f2e25aeb25906d0e9ab0d
Maksim Kavpush <mkavpush@beeline.ru>
2026-04-16 11:03:46 +0300

    tune gmaster

--- gmaster.cmd
+++ gmaster.cmd
@@ -1,1 +1,9 @@
-@git checkout master %*
+@echo off
+  git branch -a | find /i "origin/master" > nul
+  if errorlevel 1 (
+    set BASE_BRANCH=main
+  ) else (
+    set BASE_BRANCH=master
+  )
+
+git checkout %BASE_BRANCH% %*
